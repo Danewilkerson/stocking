0c2b4be775208c1874f92598cc2cc2dee2462bae
Dane Wilkerson <dane.wilkerson@knowink.com>
2022-05-23 22:50:17 -0500

    setup the stock_quote gem and api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
   def index
+    @api = StockQuote::Stock.new(api_key: 'pk_d7da50553ce54dca9907e94af4f94f17')
+
+    if params[:ticker] == ""
+      @nothing = "Please enter a symbol"
+    elsif params[:ticker]
+      begin
+        @stock = StockQuote::Stock.quote(params[:ticker])
+        @stock_logo = StockQuote::Stock.logo(params[:ticker])
+      rescue RuntimeError
+        @error = "Symbol does not exist. Please try again."
+      else 
+        p "No error"
+      ensure 
+        p "Done"
+      end
+    end
   end
+
+  def about
+  end
+
 end
