405f22dee48d8494590a04a7cdf0ac2d60588c65
Dane Wilkerson <dane.wilkerson@knowink.com>
2022-05-28 01:57:33 -0500

    finished setting up user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_25_035529) do
+ActiveRecord::Schema.define(version: 2022_05_26_221504) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_stocks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
